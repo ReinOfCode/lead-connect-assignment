9ee8814718f99b4a558b49c8dcb56a85999a6fd2
Sumit Uniyal <smtuniyall@gmail.com>
2022-11-26 23:30:46 +0530

    generate random id for user.

--- src/component/common/layout/BasicLayout.jsx
+++ src/component/common/layout/BasicLayout.jsx
@@ -1,7 +1,19 @@
+import { useUserDispatch, useUserState } from "context";
+import { generateUserId } from "context/actions";
+import { useEffect } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function BasicLayout({ children }) {
+  const userState = useUserState();
+  const dispatch = useUserDispatch();
+  const { id } = userState;
+  useEffect(() => {
+    if (!id) {
+      generateUserId(dispatch);
+    }
+  }, [id]);
+
   return (
     <>
       <div>
