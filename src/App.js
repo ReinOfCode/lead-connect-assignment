130e95003c88c21d57b923d0a06f86c51ca3d4b8
Sumit Uniyal <smtuniyall@gmail.com>
2022-11-26 23:30:20 +0530

    add context wrapper.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BasicLayout from "component/common/layout/BasicLayout";
+import UserProvider from "context";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CART_PAGE, LANDING_PAGE } from "scripts/constants";
 import CartPage from "views/CartPage";
@@ -6,14 +7,16 @@ import LandingPage from "views/LandingPage";
 
 function App() {
   return (
-    <BasicLayout>
-      <BrowserRouter>
-        <Routes>
-          <Route path={LANDING_PAGE} element={<LandingPage />}></Route>
-          <Route path={CART_PAGE} element={<CartPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </BasicLayout>
+    <UserProvider>
+      <BasicLayout>
+        <BrowserRouter>
+          <Routes>
+            <Route path={LANDING_PAGE} element={<LandingPage />}></Route>
+            <Route path={CART_PAGE} element={<CartPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </BasicLayout>
+    </UserProvider>
   );
 }
 export default App;
