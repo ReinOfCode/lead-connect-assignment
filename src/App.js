aba96f69002f0070ed0b1e85aec9c221b0a06b35
Sumit Uniyal <smtuniyall@gmail.com>
2022-11-26 18:51:12 +0530

    add routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import BasicLayout from "component/common/layout/BasicLayout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CartPage from "views/CartPage";
+import LandingPage from "views/LandingPage/Index";
 
+function App() {
+  return (
+    <BasicLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/cart-page" element={<CartPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </BasicLayout>
+  );
+}
 export default App;
