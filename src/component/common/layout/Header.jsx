e0ffacb68158887d71061aee9944b3e56d7c85f2
Sumit Uniyal <smtuniyall@gmail.com>
2022-11-27 02:52:40 +0530

    add total cart coint logic.

--- src/component/common/layout/Header.jsx
+++ src/component/common/layout/Header.jsx
@@ -1,4 +1,9 @@
+import { useUserState } from "context";
+import { CART_PRODUCT } from "scripts/constants";
+
 function Header() {
-  return <h1>Header</h1>;
+  const userState = useUserState();
+  console.log();
+  return <h1>cart count : {userState[CART_PRODUCT].length}</h1>;
 }
 export default Header;
