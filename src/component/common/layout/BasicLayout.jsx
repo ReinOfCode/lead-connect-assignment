6e1db1e282e153a19e9143b383cce3e8564e86e7
Sumit Uniyal <smtuniyall@gmail.com>
2022-11-26 18:50:54 +0530

    Fix: add return statement.

--- src/component/common/layout/BasicLayout.jsx
+++ src/component/common/layout/BasicLayout.jsx
@@ -2,12 +2,14 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 function BasicLayout({ children }) {
-  <>
-    <div className="container">
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  </>;
+  return (
+    <>
+      <div className="container">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
+  );
 }
 export default BasicLayout;
